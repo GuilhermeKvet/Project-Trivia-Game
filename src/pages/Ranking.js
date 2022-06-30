866ea43379630b872897314396863ba0d3c14bb0
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-06-30 12:03:52 -0300

    requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { reset } from '../actions';
 
 class Ranking extends React.Component {
+  resetGame = () => {
+    const { dispatch, history } = this.props;
+    dispatch(reset());
+    history.push('/');
+  }
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.resetGame }
+        >
+          Play Again
+        </button>
+      </div>
     );
   }
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Ranking);
